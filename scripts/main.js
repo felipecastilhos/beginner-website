585b04f1693938f33223e6696b79abce53a689e6
Felipe Castilhos <felipecastilhos@gmail.com>
2021-07-27 20:52:50 -0300

    feat: load image and alt dinamically from js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,38 @@
 const highlightImage = document.querySelector('img');
-const defaultHighlight = 'images/gallery_01.PNG';
 
-highlightImage.onclick = function() {
-    let mySrc = highlightImage.getAttribute('src');
-    if(mySrc === defaultHighlight) {
-        highlightImage.setAttribute('src', 'images/gallery_02.PNG');
-    } else {
-        highlightImage.setAttribute('src', defaultHighlight);
+var galleryImages = {
+    0: {
+        src: 'images/gallery_00.PNG',
+        alt: 'Imagem de Destaque: Desenho colorido de uma garota sentada em uma nuvem'
+    },
+    1: { 
+        src: 'images/gallery_01.PNG',
+        alt: 'Imagem de Destaque: Sketch de um retrato de uma garota usando um lenço sobre a cabeça em  preto e branco '
     }
+}
+
+setupGallery()
+
+function setupGallery() { 
+    loadImage(galleryImages[0])
+    setupHighlightClick()
+}
+
+function setupHighlightClick(){ 
+    const defaultHighlight = galleryImages[0].src;
+
+    highlightImage.onclick = function() {
+        let mySrc = highlightImage.getAttribute('src');
+    
+        if(mySrc === defaultHighlight) {
+            loadImage(galleryImages[1])
+        } else {
+            loadImage(galleryImages[0])
+        }
+    }
+}
+
+function loadImage(image) {
+    highlightImage.setAttribute('src', image.src);
+    highlightImage.setAttribute('alt', image.alt);
 }
